dd996bf874918695c4a8d0b2503018c782887e18
dcifligu <deivi.cifligu@gmail.com>
2025-04-16 14:48:34 +0200

    Update Supabase client configuration

--- src/lib/supabase/supabaseClient.js
+++ src/lib/supabase/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://htoujjoglfzjvyovryuc.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh0b3Vqam9nbGZ6anZ5b3ZyeXVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ2NjQ4OTksImV4cCI6MjA2MDI0MDg5OX0.vyzpeLggToGzSeVdHH3JcPL3lTSQa-kYAOYu6Pt6BXQ'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
